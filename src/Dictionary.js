7039bc7046c13713f44d46c74b094e5d47091daf
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-29 15:50:38 +0000

    css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -15,22 +15,21 @@ alert(`searching for ${keyword}`);
 }
 
 return (
-
-    <div className="Dictionary">
-      <div className="row-2">
-        <div className="col-4">
-  
-            <form onSubmit={search}>
-              <input
-                type="search"
-                onChange={handleKeywordChange}
-                autoFocus={true}
-              />
-            </form>
-            <button className="btn btn-primary">Search</button>
-
-        </div>
+  <div className="Dictionary">
+    <div className="row">
+      <div className="col-3">
+        <form onSubmit={search}>
+          <input
+            type="search"
+            onChange={handleKeywordChange}
+            autoFocus={true}
+          />
+        </form>
       </div>
+      <div className="col-3">
+        <button className="btn btn-primary">Search</button>
+      </div>
+    </div>
   </div>
 );
 }
