c7c6ed7fe8b70b7c11932d9399090084b2ed5ea2
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-30 12:35:49 +0000

    html

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,8 +4,17 @@ export default function Meaning(props) {
     return (
       <div className="Meaning">
         <h3> {props.meaning.partOfSpeech} </h3>
-        <p> {props.meaning.definition[0].definition} </p>
-        <p> {props.meaning.definition[0].example} </p>
-      </div>
+        {props.meaning.definition.map(function(definition, index) {
+     return (
+       <div key={index}>
+         <p>
+           {definition.definition}
+           <br/>
+           <em> {definition.example} </em>
+         </p>
+       </div>
+     );
+        })}
+        </div>
     );
 }
