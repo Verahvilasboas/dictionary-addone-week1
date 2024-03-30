e598d8a6f4d969bc56dc6957aafe70b8a346db6c
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-30 11:00:33 +0000

    add content

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,23 @@ import React, {useState} from "react";
 import axios from "axios";
 import "./Dictionary.css"
 import icon from "./searchicon.svg";
+import Results from "./Results.js"
 
 
 export default function Dictionary(){
 let [keyword, setkeyword] = useState ("");
+let [results, setResults] = useState (null);
+
 
 function handleResponse(response){
-    console.log(response);
+setResults(response.data[0]);
 }
 
     function search(event){
       event.preventDefault();
 
       //documentation : https://dictionaryapi.dev/
-      
+
       let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/US/${keyword}`;
       axios.get(apiUrl).then(handleResponse);
     }
@@ -40,6 +43,11 @@ return (
             placeholder="Search"
           />
         </form>
+      </div>
+      <div className="col-7">
+        <p>
+            <Results results={results} />
+        </p>
       </div>
     </div>
   </div>
