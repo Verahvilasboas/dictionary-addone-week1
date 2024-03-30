abd378076562f9ea19bc368db1bd8fb5a2190ece
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-30 11:16:10 +0000

    XX

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,8 +3,9 @@ import React from "react";
 export default function Meaning(props) {
     return (
       <div className="Meaning">
-        <h3> {props.meaning.partofSpeech} </h3>
-    <p>  {props.meaning.definition[0].definition}  </p>
+        <h3> {props.meaning.partOfSpeech} </h3>
+        <p> {props.meaning.definition[0].definition} </p>
+        <p> {props.meaning.definition[0].example} </p>
       </div>
     );
 }
