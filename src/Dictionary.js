c2918df4ea3ee1bd4869eca661239db0ec4bf683
Verahvilasboas <136754249+Verahvilasboas@users.noreply.github.com>
2024-03-31 12:04:20 +0100

    tentatives

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,55 +1,53 @@
 import React, {useState} from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css"
 import icon from "./searchicon.svg";
-import Results from "./Results"
 
 
-export default function Dictionary(){
-let [keyword, setkeyword] = useState ("");
-let [results, setResults] = useState (null);
+export default function Dictionary() {
+  let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
+  function handleResponse(response) {
+    setResults(response.data[0]);
+  }
 
-function handleResponse(response){
-setResults(response.data[0]);
-}
+  function search(event) {
+    event.preventDefault();
 
-    function search(event){
-      event.preventDefault();
+    // documentation: https://dictionaryapi.dev/e
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-      //documentation : https://dictionaryapi.dev/
-
-      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/US/${keyword}`;
-      axios.get(apiUrl).then(handleResponse);
-    }
-
-
-function handleKeywordChange(event) {
-  setkeyword(event.target.value);
-}
-
-return (
-  <div className="Dictionary">
-    <div className="row">
-      <div className="col-6">
-        <img src={icon} className="icon img-fluid" alt="icon" />
-      </div>
-      <div className="col-7">
-        <form onSubmit={search}>
-          <input
-            type="search"
-            onChange={handleKeywordChange}
-            autoFocus={true}
-            placeholder="Search"
-          />
-        </form>
-      </div>
-      <div className="col-7">
-        <p>
+  function handleKeywordChange(event) {
+    setKeyword(event.target.value);
+  }
+  return (
+    <div className="Dictionary">
+      <div className="row">
+        <div className="col-6">
+          <img src={icon} className="icon img-fluid" alt="icon" />
+        </div>
+        <div className="col-7">
+          <form onSubmit={search}>
+            <input
+              type="search"
+              placeholder="Search"
+              onChange={handleKeywordChange}
+            />
+          </form>
+          <div className="hint">
+            suggested words: sunset, wine, yoga, plant...
+          </div>
+        </div>
+        <div className="col-7">
+          <p>
             <Results results={results} />
-        </p>
+          </p>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
 }
